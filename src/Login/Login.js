fef3a7bdd20f27271d4868911d1b656b25bb00f2
Himavisi1217 <ekanyake17@gmail.com>
2024-07-28 17:53:32 +0530

    initial commit

---
+++ src/Login/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Perform login logic (authentication, etc.) here
+
+    // Assuming login is successful, navigate to AdminInterface
+    navigate('/admin');
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <h2>Admin Login</h2>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button className="login-button" onClick={handleLogin}>Login</button>
+      </div>
+      <div className="app-name">
+        CrimeWatch LK
+      </div>
+      <div className="loginlogo">
+        <img src="weblogo.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
